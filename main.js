a712ce520ee8f0f2e103e25e5c4cf849527c7063
Daniel Kindt <DanielTKindt@gmail.com>
2017-10-04 21:38:40 -0600

    finished up the project. renamed table to games for better readability.  uses jQuery to listen for the button click.

--- main.js
+++ main.js
@@ -1,42 +1,35 @@
 const STEAM_API_KEY = "43377D01563646A8748EF8FBCB0E1B7E";
 const TEST_USER = "76561197995931407";
-const ALL_GAMES = "http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/";
-const GAME_SCHEMA = "http://api.steampowered.com/ISteamUserStats/GetSchemaForGame/v2/";
-const MASTER_GAME_LIST = "http://api.steampowered.com/ISteamApps/GetAppList/v0001/";
+const ALL_GAMES_URL = "http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/";
 const USER_BANS = "http://api.steampowered.com/ISteamUser/GetPlayerBans/v1/";
 var sumPlaytime;
 
-function populateUserPlayedGames(){
-    
-    var user = getUser();
-    var url = ALL_GAMES+"?key="+STEAM_API_KEY+"&steamid="+user+"&include_appinfo=1&include_played_free_games=1&format=json";
-    var table;
-    $.ajax({
-      type: 'GET',
-      url: url,
-      contentType: 'text/plain',
-      xhrFields: {
-            withCredentials: false
-      },
-      headers: {},
-      success: function(parsed_json) {
-          var table = parsed_json['response']['games'];
-          processUserGames(table);
-      },
-      error: function() {
-          alert("REST SERVICE ERROR");
-      }
-   });
+function populateUserPlayedGames(userID){
+    console.log("Getting stats for " + userID);
+    var params = "?key=" + STEAM_API_KEY + "&steamid=" + userID.toString() + "&format=json" +
+        "&include_appinfo=1&include_played_free_games=1";
+    var url = ALL_GAMES_URL + params;
+    $.get(url)
+        .done(function ( data ) {
+            console.log(this.url);
+            var games = data['response']['games'];
+            console.log(games);
+            processUserGames(games);
+            hasBans(userID);
+        })
+        .fail(function () {
+            console.warn("Failed to make the REST call!");
+            alert("API call failed!");
+        })
 }
 
-function processUserGames(table){  
+function processUserGames(games){
     sumPlaytime=0;
-    hasBans();
-    table.sort(function(a, b) {
+    games.sort(function(a, b) {
         return parseFloat(a.playtime_forever) - parseFloat(b.playtime_forever);
     });
-    table.reverse();
-    updatePage(table);               
+    games.reverse();
+    updatePage(games);
 }
 
 function playerBanned(){
@@ -49,8 +42,7 @@ function playerNeverBanned(){
 	$("#bans").html("User is a good community member! Never banned!");
 }
 
-function hasBans(){
-	var user = getUser();
+function hasBans(user) {
 	url = USER_BANS+"?key="+STEAM_API_KEY+"&steamids="+user+"&format=json";
 	$.ajax({
       type: 'GET',
@@ -79,41 +71,45 @@ function hasBans(){
 }
 
 //This function updates the page with the information we now know from all our HTTP requests. We pass in the finished table when we're done getting crap.
-function updatePage(table){
+function updatePage(games){
     var output ="";
     console.log("Time to update the page");
-    for(var i =0;i<table.length;i++){
+    for(var i =0;i<games.length;i++){
         //We only want to include games the user has actually played.
-        var gameTime = table[i]["playtime_forever"];
+        var gameTime = games[i]["playtime_forever"];
         if(gameTime>0){
-            table[i]['played']=(gameTime/60).toFixed(2);
-            output += addRow(table, i);
+            games[i]['played']=(gameTime/60).toFixed(2);
+            output += addRow(games, i);
             sumPlaytime+=gameTime;
         }
     }
     $("#output-table").html(output);
 }
 
-function addRow(table, i){
+function addRow(games, i){
     var output ="<tr><th scope=&quot;row&quot;>";
-    output+=table[i]["name"];
+    output+=games[i]["name"];
     output+="</th><td>";
-    output+=table[i]["played"];
+    output+=games[i]["played"];
     output+=" hours</td><td>";
-    output+=table[i]["numAchievements"];
+    output+=games[i]["numAchievements"];
     output+="</td></tr>";
     return output;
 }
 
-//This method makes sure the user isn't blank. If it is, we use the test user instead.
-function getUser(){
+$("#search-username").click(function(){
     var user = $("#username-input").val();
-    if (user ==null || user==""||user.length!=23){
-        return TEST_USER;
+    if (user === "") {
+        populateUserPlayedGames(TEST_USER)
     }
-    return user;
-}
-
-$(document).ready(function() {
-    populateUserPlayedGames();
+    var url = "http://steamcommunity.com/id/" + user.toLowerCase() + "?xml=1";
+    console.log("url: " + url);
+    $.get(url, function(data, status){
+        if (data.getElementsByTagName("error").length > 0) {
+            console.warn("User does not exist!");
+        }
+        var steamID = data.getElementsByTagName("steamID64")[0].childNodes[0].data;
+        console.log(steamID);
+        populateUserPlayedGames(steamID);
+    });
 });
