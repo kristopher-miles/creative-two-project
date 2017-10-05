b10b803671bde35cac0beae1e024cbd8786447c6
Kristopher Miles <kristophermiles@gmail.com>
2017-10-04 18:29:47 -0600

    Refactored to use a single efficent call. Yay for speed!

--- main.js
+++ main.js
@@ -6,10 +6,10 @@ const MASTER_GAME_LIST = "http://api.steampowered.com/ISteamApps/GetAppList/v000
 const USER_BANS = "http://api.steampowered.com/ISteamUser/GetPlayerBans/v1/";
 var sumPlaytime;
 
-function getAllGames(){
+function populateUserPlayedGames(){
     sumPlaytime=0;
     var user = getUser();
-    var url = ALL_GAMES+"?key="+STEAM_API_KEY+"&steamid="+user+"&format=json";
+    var url = ALL_GAMES+"?key="+STEAM_API_KEY+"&steamid="+user+"&include_appinfo=1&include_played_free_games=1&format=json";
     var table;
     $.ajax({
       type: 'GET',
@@ -21,12 +21,29 @@ function getAllGames(){
       headers: {},
       success: function(parsed_json) {
           var table = parsed_json['response']['games'];
-          newUpdateTable(table);
+          processUserGames(table);
       },
       error: function() {
-          alert("Kill me now.");
+          alert("REST SERVICE ERROR");
       }
    });
+}
+
+function processUserGames(table){  
+    for(var i =0;i<table.length;i++){
+        var gameTime = table[i].playtime_forever;
+        console.log(gameTime);
+        if(gameTime>0){
+            table[i]['played']=(gameTime/60).toFixed(2);
+            sumPlaytime+=gameTime;
+        }
+    }
+    hasBans();
+    table.sort(function(a, b) {
+        return parseFloat(a.playtime_forever) - parseFloat(b.playtime_forever);
+    });
+    table.reverse();
+    updatePage(table);               
 }
 
 function playerBanned(){
@@ -68,57 +85,16 @@ function hasBans(){
 	
 }
 
-function newUpdateTable(userGamesTable){                
-    url = MASTER_GAME_LIST+"?key="+STEAM_API_KEY+"&format=json";
-    $.ajax({
-      type: 'GET',
-      url: url,
-      contentType: 'text/plain',
-      xhrFields: {
-            withCredentials: false
-      },
-      headers: {},
-      success: function(parsed_json) {
-          var table = parsed_json['applist']['apps']['app'];
-          var outputAppsTable=[];
-          for(var i =0;i<table.length;i++){
-              var id =table[i]['appid'];
-              //console.log("added game id: "+id+" with name: "+table[i]['name']);
-              outputAppsTable[id]=table[i]['name'];
-          }
-         //Now that we have all the game names, we can add them correctly.
-          for(var i =0;i<userGamesTable.length;i++){
-              if(userGamesTable[i]["playtime_forever"]>0){
-                  var gameID=userGamesTable[i]["appid"];
-                  userGamesTable[i]['name']=outputAppsTable[gameID];
-                  userGamesTable[i]['played']=(userGamesTable[i]["playtime_forever"]/60).toFixed(2);
-                  userGamesTable[i]['numAchievements']=0;
-              }
-          }
-          
-          //Finally, update the output table.
-		  hasBans();
-		  userGamesTable.sort(function(a, b) {
-    		return parseFloat(a.playtime_forever) - parseFloat(b.playtime_forever);
-		  });
-		  userGamesTable.reverse();
-          updatePage(userGamesTable);
-        
-      },
-      error: function() {
-          alert("Kill me now.");
-      }
-   }); 
-                        
-}
-
 //This function updates the page with the information we now know from all our HTTP requests. We pass in the finished table when we're done getting crap.
 function updatePage(table){
     var output ="";
+    console.log("Time to update the page");
     for(var i =0;i<table.length;i++){
         //We only want to include games the user has actually played.
         if(table[i]["playtime_forever"]>0){
+            console.log("This game has playtime. "+table[i]['name']+" "+table[i]['played']+" time.");
             output += addRow(table, i);
+            
         }
     }
     $("#output-table").html(output);
@@ -138,7 +114,6 @@ function addRow(table, i){
 //This method makes sure the user isn't blank. If it is, we use the test user instead.
 function getUser(){
     var user = $("#username-input").val();
-    
     if (user ==null || user==""||user.length!=23){
         return TEST_USER;
     }
@@ -146,5 +121,5 @@ function getUser(){
 }
 
 $(document).ready(function() {
-    getAllGames();
+    populateUserPlayedGames();
 });
